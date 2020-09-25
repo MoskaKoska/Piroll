c4c3fe9b1bfa673dbfc096fb6412928e9d961765
MoskaKoska <30430308+MoskaKoska@users.noreply.github.com>
2020-09-25 21:46:52 +0500

    Update scripts.js
    .

--- js/scripts.js
+++ js/scripts.js
@@ -8,63 +8,7 @@ window.onscroll = function () {
   }
 };
 
-// function slider(selector) {
-//   let slider = $(selector);
-//   let items = slider.children();
-//
-//   slider
-//     addClass('slider')
-//     .append('<div class="slider__slides"></div>')
-//     .append('<div class="slider__dots"></div>')
-//     .on('click', 'slider__dot', function (e) {
-//       e.preventDefault();
-//
-//       let a = $(this);
-//       let active = slider.find('slider__slide_active');
-//       let current = active.index();
-//       let next = current;
-//
-//       if (a.hasClass('slider__dot')) {
-//         next = a.index();
-//       };
-//
-//       if (current == next) {
-//         return;
-//       }
-//
-//       active.removeClass('slider__slide_active');
-//       slider
-//         .find('slider__dot_active')
-//         .removeClass('slider__dot_active');
-//       items
-//         .eq(next)
-//         .addClass('slider__slide_active');
-//       slider
-//         .find('slider__dot')
-//         .eq(next)
-//         .addClass('slider__dot_active')
-//     });
-//
-//   let slides = slider.children('.slider__slides');
-//   let dots = slider.children('.slider__dots');
-//
-//   items
-//     .prependTo(slides)
-//     .each(function (i) {
-//
-//       if (!i) {
-//         dots.append('<a href="#" class="slider__dot slider__dot_active"></a>');
-//       } else {
-//         dots.append('<a href="#" class="slider__dot');
-//       }
-//
-//     })
-//     .addClass('slider__slide')
-//     .eq(0)
-//     .addClass('slider__slide_active');
-// }
-//
-// slider('#slider');
+
 
 function slider(selector) {
     let slider = $(selector);
